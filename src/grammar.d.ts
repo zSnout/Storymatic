89033f839b23f8c5e97700292f9b6d44d595a232
Zachary <zsakowitz@zsnout.com>
2022-04-28 08:43:37 -0700

    Remove unused parsers

--- src/grammar.d.ts
+++ src/grammar.d.ts
@@ -250,10 +250,6 @@ export interface StorymaticActionDict<T> extends ActionDict<T> {
   string_bit?: (this: NonterminalNode, arg0: NonterminalNode) => T;
   string_interpolatable_head?: (this: NonterminalNode, arg0: IterationNode) => T;
   string_interpolatable_span?: (this: NonterminalNode, arg0: TerminalNode, arg1: Node, arg2: TerminalNode, arg3: IterationNode) => T;
-  string_interpolatable_bit_interpolated?: (this: NonterminalNode, arg0: TerminalNode, arg1: NonterminalNode, arg2: TerminalNode) => T;
-  string_interpolatable_bit?: (this: NonterminalNode, arg0: NonterminalNode) => T;
-  string_type_interpolatable_bit_interpolated?: (this: NonterminalNode, arg0: TerminalNode, arg1: NonterminalNode, arg2: TerminalNode) => T;
-  string_type_interpolatable_bit?: (this: NonterminalNode, arg0: NonterminalNode) => T;
   string_interpolatable?: (this: NonterminalNode, arg0: Node, arg1: NonterminalNode, arg2: IterationNode, arg3: Node) => T;
   string_full?: (this: NonterminalNode, arg0: Node, arg1: IterationNode, arg2: Node) => T;
   string_non_interpolatable?: (this: NonterminalNode, arg0: NonterminalNode) => T;
