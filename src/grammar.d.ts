07c1afd69f7b30a7f302976dcd383d23988719a0
Zachary <zsakowitz@zsnout.com>
2022-05-15 06:53:28 -0700

    Reorder yield and yield-from

--- src/grammar.d.ts
+++ src/grammar.d.ts
@@ -202,8 +202,8 @@ export interface StorymaticActionDict<T> extends ActionDict<T> {
   AssignmentExp_assignment?: (this: NonterminalNode, arg0: NonterminalNode, arg1: TerminalNode, arg2: NonterminalNode) => T;
   AssignmentExp_splice?: (this: NonterminalNode, arg0: NonterminalNode, arg1: TerminalNode, arg2: IterationNode, arg3: TerminalNode, arg4: IterationNode, arg5: TerminalNode, arg6: TerminalNode, arg7: NonterminalNode) => T;
   AssignmentExp_update_assignment?: (this: NonterminalNode, arg0: NonterminalNode, arg1: TerminalNode, arg2: NonterminalNode) => T;
-  AssignmentExp_yield?: (this: NonterminalNode, arg0: TerminalNode, arg1: NonterminalNode, arg2: IterationNode) => T;
   AssignmentExp_yield_from?: (this: NonterminalNode, arg0: TerminalNode, arg1: NonterminalNode, arg2: TerminalNode, arg3: NonterminalNode, arg4: NonterminalNode) => T;
+  AssignmentExp_yield?: (this: NonterminalNode, arg0: TerminalNode, arg1: NonterminalNode, arg2: IterationNode) => T;
   AssignmentExp?: (this: NonterminalNode, arg0: NonterminalNode) => T;
   ForExp_for?: (this: NonterminalNode, arg0: NonterminalNode, arg1: TerminalNode, arg2: NonterminalNode, arg3: IterationNode, arg4: IterationNode, arg5: IterationNode, arg6: IterationNode, arg7: IterationNode, arg8: NonterminalNode, arg9: IterationNode, arg10: IterationNode, arg11: IterationNode) => T;
   ForExp_print?: (this: NonterminalNode, arg0: TerminalNode, arg1: NonterminalNode, arg2: NonterminalNode) => T;
